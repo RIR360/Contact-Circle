9da40ae051d194a1fe2687d126ee007454ad141a
rir360 <rezowanislamrizvy7@gmail.com>
2023-09-19 01:16:47 +0600

    alert box white theme

--- Client/src/component/Alert.jsx
+++ Client/src/component/Alert.jsx
@@ -2,7 +2,7 @@ export default function Alert({ message }) {
 
     return (
         <div className={`
-        bg-gray-100 border-t-4 border-orange-500 rounded-b 
+        border-t-4 border-orange-500 rounded-b 
         px-4 py-3 shadow-md my-3
     `} role="alert">
             <p className="font-bold">
