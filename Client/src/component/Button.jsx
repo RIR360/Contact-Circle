1d93b1cb4a5b380346386fcb72a5ccc0c0306856
rir360 <rezowanislamrizvy7@gmail.com>
2023-09-19 01:16:30 +0600

    button template accepting type property

--- Client/src/component/Button.jsx
+++ Client/src/component/Button.jsx
@@ -1,11 +1,11 @@
-export default function Button({handleClick, children}) {
+export default function Button({handleClick, type, children}) {
 
     return (
         <button className="
             p-2 px-3 rounded
             hover:bg-gray-300
             fade-in
-        " onClick={handleClick}>
+        " onClick={handleClick} type={type}>
             {children}
         </button>
     )
