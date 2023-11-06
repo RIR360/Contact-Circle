151a524152eefd82d4cd4896ceafd45632967c63
rir360 <rezowanislamrizvy7@gmail.com>
2023-11-07 01:18:48 +0600

    Mobile screen scrolling bug fixed

--- Client/src/component/Level.jsx
+++ Client/src/component/Level.jsx
@@ -1,25 +1,26 @@
 import Avatar from "./Avatar";
 export default function Level({level, opener, children}) {
 
-    if (!children) return <></>;
+  if (!children) return <></>;
 
-    return (
-        <>
-            <div className="mt-3">
-                <span className="text-sm"> Level {level}</span>
-            </div>
-            <div className="
-                p-4 text-center flex justify-center gap-10
-                overflow-auto w-full transition 
-                border-l-4 hover:border-orange-600
-            ">
-                {children.map((contact) =>
+  return (
+    <>
+      <div className="mt-3">
+          <span className="text-sm"> Level {level}</span>
+      </div>
+      <div className="
+          p-4 overflow-auto flex justify-center
+          transition border-l-4 hover:border-orange-600 
+      ">
+        <div className="flex text-center gap-10">
+          {children.map((contact) =>
 
-                    <Avatar key={contact._id} data={contact} open={opener} />
+            <Avatar key={contact._id} data={contact} open={opener} />
 
-                )}
-            </div>
-        </>
-    )
+          )}
+        </div>
+      </div>
+    </>
+  )
 
 }
